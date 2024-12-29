ac6e8423a18849b838057b422a3527c0ac9c3e4f
Maxime188 <maxime.couturier@ensea.fr>
2024-12-29 18:15:30 +0100

    Add files via upload

---
+++ TP_synthese2/main1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "gettftp.h"
+#include "puttftp.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+
+int main(int argc, char *argv[]){
+
+    if (argc<4){
+        printf("Usage: %s [gettftp|puttftp] <server> <file>\n", argv[0]);
+        return 1;
+    }
+
+
+    const char *command = argv[1];
+    const char *server = argv[2];
+    const char *filename = argv[3];
+
+    if (strcmp(command, "gettftp")==0){
+        gettftp(server, filename);
+        return 0;
+    }
+
+    else if (strcmp(command, "puttftp")==0){
+        puttftp(server, filename);
+        return 0;
+    }
+    else{
+        printf("Commande inconnue: %s \n", command);
+        return 1;
+    }
+    return 0;
+}
--- TP_synthese2/main2.c
+++ TP_synthese2/main2.c
@@ -1,84 +1,92 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "gettftp.h"
-#include "puttftp.h"
-
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
+#define BUF_SIZE 500
 
 int main(int argc, char *argv[]){
+    int              sfd, s;
+    char             buf[BUF_SIZE];
+    size_t           len;
+    ssize_t          nread;
+    struct addrinfo  hints;
+    struct addrinfo  *result, *rp;
+    char ipstr[INET6_ADDRSTRLEN]; //to stock the address
 
-    if (argc<4){
-        printf("Usage: %s [gettftp|puttftp] <server> <file>\n", argv[0]);
-        return 1;
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s host port msg...\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
 
+    /* Obtain address(es) matching host/port. */
 
-    const char *command = argv[1];
-    const char *server = argv[2];
-    const char *filename = argv[3];
-
-    if (strcmp(command, "gettftp")==0){
-        gettftp(server, filename);
-        return 0;
-    }
-
-    else if (strcmp(command, "puttftp")==0){
-        puttftp(server, filename);
-        return 0;
-    }
-    else{
-        printf("Commande inconnue: %s \n", command);
-        return 1;
-    }
-    return 0;
-
-    
-
-    const char *hostname = "serveur";
-    const char *port = "80";
-
-    struct addrinfo hints, *res, *p;
-    int status;
-
-    //Initialization of hints structure
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;         // IPv4 or IPv6
-    hints.ai_socktype = SOCK_DGRAM;     // Socket UDP
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;          /* Any protocol */
 
-    status = getaddrinfo(hostname, port, &hints, &res);
-    if (status != 0) {
-        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
-        return 1;
+    s = getaddrinfo(argv[1], argv[2], &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
     }
 
-    // Parcourir les résultats
-    printf("Adresses pour %s:\n", hostname);
-    for (p = res; p != NULL; p = p->ai_next) {
-        char ipstr[INET6_ADDRSTRLEN];
-        void *addr;
-        char *ipver;
-        // Obtenir le pointeur vers l'adresse (IPv4 ou IPv6)
-        if (p->ai_family == AF_INET) { // IPv4
-            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
-            addr = &(ipv4->sin_addr);
-            ipver = "IPv4";
-        } else { // IPv6
-            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-            addr = &(ipv6->sin6_addr);
-            ipver = "IPv6";
+    /* getaddrinfo() returns a list of address structures.
+        Try each address until we successfully connect(2).
+        If socket(2) (or connect(2)) fails, we (close the socket and) try the next address. */
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        if (rp->ai_family == AF_INET) {  // IPv4
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)rp->ai_addr; //conversion of the binary address 
+            inet_ntop(AF_INET, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
+             printf("Adresse IPv4 du serveur : %s\n", ipstr);
         }
-        // Convertir l'adresse en chaîne de caractères
-        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
-        printf("  %s: %s\n", ipver, ipstr);
+        else if (rp->ai_family == AF_INET6) {  // IPv6
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)rp->ai_addr;
+            inet_ntop(AF_INET6, &(ipv6->sin6_addr), ipstr, sizeof(ipstr));
+            printf("Adresse IPv6 du serveur : %s\n", ipstr);
+        }
+
+        //socket creation :
+
+        
+        sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sfd == -1) {
+            perror("socket");
+            continue;
+        }
+        
+
+       //connexion to the server :
+       
+       
+        if (connect(sfd, rp->ai_addr, rp->ai_addrlen) == -1) {
+            close(sfd);
+            perror("connect");
+            continue;
+        }
+        
+
     }
+   
 
-    // Libérer la mémoire allouée par getaddrinfo
-    freeaddrinfo(res);
+    freeaddrinfo(result);           /* No longer needed */
 
+    if (rp == NULL) {               /* No address succeeded */
+        fprintf(stderr, "Could not connect\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Succès\n");
+
+    //closure of the socket after using it :
+
+    close(sfd);
     return 0;
+
 }
