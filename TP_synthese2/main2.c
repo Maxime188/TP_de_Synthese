662ef8f8640c34345fd5435094d8d62c8a327539
Maxime188 <maxime.couturier@ensea.fr>
2024-12-29 13:51:30 +0100

    Add files via upload

--- TP_synthese2/main2.c
+++ TP_synthese2/main2.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "gettftp.h"
 #include "puttftp.h"
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
 
 int main(int argc, char *argv[]){
 
@@ -31,4 +38,47 @@ int main(int argc, char *argv[]){
     return 0;
 
     
+
+    const char *hostname = "serveur";
+    const char *port = "80";
+
+    struct addrinfo hints, *res, *p;
+    int status;
+
+    //Initialization of hints structure
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;         // IPv4 or IPv6
+    hints.ai_socktype = SOCK_DGRAM;     // Socket UDP
+
+    status = getaddrinfo(hostname, port, &hints, &res);
+    if (status != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        return 1;
+    }
+
+    // Parcourir les résultats
+    printf("Adresses pour %s:\n", hostname);
+    for (p = res; p != NULL; p = p->ai_next) {
+        char ipstr[INET6_ADDRSTRLEN];
+        void *addr;
+        char *ipver;
+        // Obtenir le pointeur vers l'adresse (IPv4 ou IPv6)
+        if (p->ai_family == AF_INET) { // IPv4
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else { // IPv6
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+        // Convertir l'adresse en chaîne de caractères
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        printf("  %s: %s\n", ipver, ipstr);
+    }
+
+    // Libérer la mémoire allouée par getaddrinfo
+    freeaddrinfo(res);
+
+    return 0;
 }
